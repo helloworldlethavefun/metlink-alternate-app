e3c2b375ea763d9b5cd0a0b7c0c8cbea7349672e
helloworldlethavefun <bluefloyd12321@gmail.com>
2024-01-08 23:58:29 +0000

    Tidying up some things

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ app = Flask(__name__)
 app.secret_key = 'ASuperSecretKey'
 stop_names_n = 0
 
+
+# Define a form that collects the stopname 
 class AddStopsForm(FlaskForm):
     stop = StringField('stop_name', validators=[DataRequired()])
-
 
 
 # load in all the stops information
@@ -32,11 +33,9 @@ def load_json_data():
         data = json.loads(file.read())
         file.close
 
-
-
+# Just checks if the json file is empty or exists. 
 def is_json_file_empty(file):
     return os.path.exists(file) and os.stat(file).st_size == 0
-
 
 
 # ask the user for the name of the stop they wish to obtain the id of then add to the stops list
@@ -44,10 +43,9 @@ def get_stop_id(stop_name):
     global stop_n
     for dictionary in data:
         if stop_name in dictionary.values():
-            print(dictionary.values())
             stop_id = dictionary["stop_id"]
             stop_n = int(stop_n)
-            stop_n = stop_n + 1
+            stop_n += 1
             stops[stop_n] = stop_id
 
 # Vice-versa of the function above. Take the stop id and find the name of the stop
@@ -56,6 +54,8 @@ def get_stop_name(stop_id):
         if stop_id in dictionary.values():
             stop_name = dictionary["stop_name"]
             return stop_name
+
+
 
 # Write all of the stops to be monitored to a file
 def write_list_to_file():
@@ -83,6 +83,7 @@ def pull_list_from_file():
         file.close
 
 
+# Query the opendata api and pulls down the predictions of the arrival of the bus
 def get_stop_predictions(stop_id):
     payload = {'stop_id': stop_id}
     headers = {'accept': 'application/json', 'x-api-key': Api_key}
@@ -92,7 +93,8 @@ def get_stop_predictions(stop_id):
     expected_time = datass['departures'][0]['arrival']
     return expected_time
 
-
+# This is the main page. It takes the stops you wish to monitor and then find the times 
+# and display them
 @app.route("/")
 def index():
     try:
@@ -107,42 +109,21 @@ def index():
         time = time['aimed']
         parsed_time = parser.parse(time)
         time = parsed_time.strftime("%H:%M:%S")
-        print(time)
         times[stop_nn] = time
-        print(times)
     return render_template('index.html', times=times)
 
-
-
-@app.route("/add-stops", methods=['GET', 'POST'])
+# This is the page where users can add/remove stops from the list
+@app.route("/manage-stops", methods=['GET', 'POST'])
 def managing_stops():
     form = AddStopsForm()
     if form.validate_on_submit():
         data = form.stop.data
         stop_ids = get_stop_id(data)
-        print(stop_ids)
         write_list_to_file()
-        print(stops)
     return render_template('add_stops.html', form=form)
 
-@app.route("/stops")
-def listing_stops():
-        stop_names = {}
-        try:
-            pull_list_from_file()
-            print(stops)
-            stop_names_n = 0
-        except:
-            return "Looks like you don't have any stops here. Want to add them? Go <a href='/add-stops'>here</a>"
-        for item in stops.values():
-            stop_name = get_stop_name(item)
-            stop_names_n += 1
-            stop_names[stop_names_n] = stop_name
-            print(stop_names)
-        return render_template('list_stops.html', stops=stop_names)
 
-
-
+# When the program is ran, import all of the information of the stops and start flask
 if __name__ == "__main__":
     load_json_data()
-    app.run(port=8008, host='0.0.0.0', debug=True)
+    app.run(port=8008, host='0.0.0.0')
